266825e4065c2bc2a9571a62b3d12dde7fea7a88
Lincoln Amorim <54185019+lolincoln@users.noreply.github.com>
2022-06-13 15:35:18 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <string.h>
 
+int main(int argc, char *argv[]) {  
+    if (argc != 3) {
+        printf("Uso: nome-arquivo num-thread\n");
+        return 0;
+    }
+
+    FILE *fw_bin = NULL;
+    FILE *fl_bin = NULL;
+    char* nread, nwrite;
+  
+    strcpy(nread,argv[1]);
+    strcpy(nwrite,argv[1]);
+    strcat(nread,".mips");
+    strcat(nwrite,".bin");
+  
+    fl_bin = fopen(nread, "rb");
+    if (fl_bin == NULL) {
+        printf("Arquivo nao encontrado\n");
+        return 0;
+    }
+  
+  
+    printf("Arquivo lido\n");
+    fclose(fl_bin);
+  
+    
+    fw_bin = fopen(nwrite, "wb");
+    if (fw_bin == NULL) {
+        perror("Erro ao criar o arquivo\n");
+        return 0;
+    }
+    fclose(fw_bin);
+  
+    return 1;
+}
